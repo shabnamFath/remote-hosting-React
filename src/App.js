2e32abfa1dd360a542e16177b3fc0123db996d4f
Borhaan Hnia <borhaan@Borhaans-MacBook-Pro.local>
2018-10-30 17:30:33 -0400

    newer version

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
+    return (<div>
       <h1>Hello from React</h1>
+      <h2>updating react</h2>
+      </div>
     );
   }
 }
